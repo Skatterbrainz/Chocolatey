94df622d379ed9224ac30dc5d592831005c93455
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2017-06-29 21:27:41 -0400

    Create Update-ChocoApps.ps1

---
+++ Update-ChocoApps.ps1
@@ -0,0 +1,23 @@
+<# 
+.SYNOPSIS
+  Upgrade local chocolatey packages if network connection is found
+
+.NOTES
+  Place shortcut to script in Start Menu / Startup, or 
+  create On-Login scheduled task, etc.
+  
+  Author: David Stein
+  Date Created: 06/05/2017
+#>
+
+if (Get-NetAdapter | Where-Object {$_.Status -eq 'Up'}) {
+    $choice = Read-Host `n "Update Chocolatey packages now [Y/N]"
+    if ($choice -eq 'Y') {
+	    choco upgrade all -y
+	    Write-Host "Packages have been updated." -ForegroundColor Green
+    }
+    Write-Output "Completed."
+}
+else {
+    Write-Output "No active network connection found."
+}
