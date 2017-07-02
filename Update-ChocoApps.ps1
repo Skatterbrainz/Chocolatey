55c0e1b5500438ad78a0b5f07a31775eb4b5350d
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2017-07-01 23:13:07 -0400

    Update Update-ChocoApps.ps1

--- Update-ChocoApps.ps1
+++ Update-ChocoApps.ps1
@@ -11,13 +11,14 @@
 #>
 
 if (Get-NetAdapter | Where-Object {$_.Status -eq 'Up'}) {
-    $choice = Read-Host `n "Update Chocolatey packages now [Y/N]"
-    if ($choice -eq 'Y') {
-	    choco upgrade all -y
-	    Write-Host "Packages have been updated." -ForegroundColor Green
-    }
-    Write-Output "Completed."
+	Write-Host "Active network connection verified" -ForegroundColor Green 
+	$choice = Read-Host `n "Update Chocolatey packages now [Y/N]"
+	if ($choice -eq 'Y') {
+		choco upgrade all -y
+		Write-Host "Packages have been updated." -ForegroundColor Green
+	}
+	Write-Output "Completed."
 }
 else {
-    Write-Output "No active network connection found."
+	Write-Output "No active network connection found."
 }
