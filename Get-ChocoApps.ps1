7e26fbe8ca84cfcfdd96e8d7654c34b5939e7ef1
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2017-08-04 01:47:34 -0400

    Update Get-ChocoApps.ps1

--- Get-ChocoApps.ps1
+++ Get-ChocoApps.ps1
@@ -3,15 +3,28 @@
   Returns installed chocolatey packages from 
   one or more remote computers in a single output
   which can be filtered/searched
+
 .PARAMETER Computer
   [string] (required) Name of computer(s) to query
+
 .NOTES
   Returns a hash table with [Computer] and [Package] keys
   so you can filter results by either if piped into
   Where-Object {$_.<keyname> <operator> <value>}
+
 .EXAMPLE
+  # filter for computers having package names beginning with "notepad"
   $results = Get-ChocoApps.ps1 -Computer D001,D002 
   $results | Where-Object {$_.Package -like 'notepad*'}
+
+.EXAMPLE
+  # filter for packages on computer D002
+  $results = Get-ChocoApps.ps1 -Computer D001,D002
+  $results | ? {$_.Computer -eq 'D002'} | % {_.Package}
+
+.EXAMPLE
+  # filter unique package names from all computers
+  $results | % {$_.Package} | Select -Unique
 #>
 
 param (
