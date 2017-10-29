be4f468a75d274273534947feb366f95150bb98d
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2017-10-29 03:08:10 -0400

    Update Invoke-FudgePack-002.ps1

--- Invoke-FudgePack-002.ps1
+++ Invoke-FudgePack-002.ps1
@@ -14,9 +14,13 @@
 	
 .PARAMETER TestMode
 	[optional][switch] WhatIf mode - no installs or removals executed
+
+.PARAMETER Payload
+	[optional] [string] One of 'All', 'Files', 'Folders', 'Registry', 'Installs', 'Removals' or 'Services'
+	default is 'All'
 	
 .NOTES
-	1.0.2 - 10/28/2017 - skatterbrainz
+	1.0.5 - 10/29/2017 - skatterbrainz
 	
 .EXAMPLE
 	
@@ -28,17 +32,17 @@
 
 .EXAMPLE
 	
-	Invoke-FudgePack -TestMode -Verbose
+	Invoke-FudgePack -TestMode -Payload 'Registry' -Verbose
 	
 #>
 
 param (
 	[parameter(Mandatory=$False)]
 		[ValidateNotNullOrEmpty()]
-		[string] $ControlFile = 'https://raw.githubusercontent.com/Skatterbrainz/Chocolatey/master/fudgepack/control.xml',
+		[string] $ControlFile = 'https://raw.githubusercontent.com/Skatterbrainz/Chocolatey/master/control.xml',
 	[parameter(Mandatory=$False)]
 		[ValidateNotNullOrEmpty()]
-		[string] $LogFile = "$($env:TEMP)\fudgepack.log",
+		[string] $LogFile = "$($env:ProgramData)\fudgepack\fudgepack.log",
 	[parameter(Mandatory=$False)]
 		[switch] $TestMode,
 	[parameter(Mandatory=$False)]
@@ -71,7 +75,8 @@ function Write-FudgePackLog {
 	}
 	else {
 		Write-Verbose "$(Get-Date -f 'yyyy-M-dd HH:mm:ss')  $Category  $Message"
-		"$(Get-Date -f 'yyyy-M-dd HH:mm:ss')  $Category  $Message" | Out-File $LogFile -Append -NoClobber -Encoding Default
+		"$(Get-Date -f 'yyyy-M-dd HH:mm:ss')  $Category  $Message" | 
+			Out-File $LogFile -Append -NoClobber -Encoding Default
 	}
 }
 
@@ -89,13 +94,12 @@ function Invoke-FPChocoInstalls {
 	)
 	Write-FudgePackLog -Category "Info" -Message "--------- installaation assignments ---------"
 	if ($DataSet) {
-		$devicename = $DataSet.device
+		$deviceName = $DataSet.device
 		$runtime    = $DataSet.when
 		$autoupdate = $DataSet.autoupdate
 		$username   = $DataSet.user
 		$extparams  = $DataSet.params
-		Write-FudgePackLog -Category "Info" -Message "installations have been assigned to this computer"
-		Write-FudgePackLog -Category "Info" -Message "assigned device: $devicename"
+		Write-FudgePackLog -Category "Info" -Message "assigned to device: $deviceName"
 		Write-FudgePackLog -Category "Info" -Message "assigned runtime: $runtime"
 		if ($runtime -eq 'now' -or (Get-Date).ToLocalTime() -ge $runtime) {
 			Write-FudgePackLog -Category "Info" -Message "run: runtime is now or already passed"
@@ -137,12 +141,11 @@ function Invoke-FPChocoRemovals {
 	)
 	Write-FudgePackLog -Category "Info" -Message "--------- removal assignments ---------"
 	if ($DataSet) {
-		$devicename = $DataSet.device
+		$deviceName = $DataSet.device
 		$runtime    = $DataSet.when
 		$username   = $DataSet.user
 		$extparams  = $DataSet.params
-		Write-FudgePackLog -Category "Info" -Message "removals have been assigned to this computer"
-		Write-FudgePackLog -Category "Info" -Message "assigned device: $devicename"
+		Write-FudgePackLog -Category "Info" -Message "assigned to device: $deviceName"
 		Write-FudgePackLog -Category "Info" -Message "assigned runtime: $runtime"
 		if ($runtime -eq 'now' -or (Get-Date).ToLocalTime() -ge $runtime) {
 			Write-FudgePackLog -Category "Info" -Message "run: runtime is now or already passed"
@@ -154,16 +157,16 @@ function Invoke-FPChocoRemovals {
 						choco uninstall $pkg $extparams
 					}
 					else {
-						Write-Verbose "TEST MODE : $pkg"
+						Write-Verbose "TEST MODE : choco uninstall $pkg $extparams"
 					}
 				}
 				else {
 					Write-FudgePackLog -Category "Info" -Message "package: $pkg"
 					if (-not $TestMode) {
-						choco uninstall $pkg -y
+						choco uninstall $pkg -y -r
 					}
 					else {
-						Write-Verbose "TEST MODE : $pkg"
+						Write-Verbose "TEST MODE : choco uninstall $pkg -y -r"
 					}
 				}
 			} # foreach
@@ -191,6 +194,8 @@ function Invoke-FPRegistry {
 			$regval     = $reg.value
 			$regdata    = $reg.data
 			$regtype    = $reg.type
+			$deviceName = $reg.device
+			Write-FudgePackLog -Category "Info" -Message "assigned to device: $deviceName"
 			Write-FudgePackLog -Category "Info" -Message "keypath: $regpath"
 			Write-FudgePackLog -Category "Info" -Message "value: $regval"
 			Write-FudgePackLog -Category "Info" -Message "data: $regdata"
@@ -224,28 +229,44 @@ function Invoke-FPServices {
 	)
 	Write-FudgePackLog -Category "Info" -Message "--------- services assignments ---------"
 	foreach ($service in $DataSet) {
-		$svcName   = $service.name
-		$svcStart  = $service.startup
-		$svcAction = $service.action
+		$svcName    = $service.name
+		$svcStart   = $service.startup
+		$svcAction  = $service.action
+		$deviceName = $service.device
+		Write-FudgePackLog -Category "Info" -Message "assigned to device: $deviceName"
 		Write-FudgePackLog -Category "Info" -Message "service name: $svcName"
 		Write-FudgePackLog -Category "Info" -Message "startup should be: $svcStart"
 		Write-FudgePackLog -Category "Info" -Message "requested action: $svcAction"
 		try {
 			$scfg = Get-Service -Name $svcName
-			if ($scfg.StartType -ne $svcStart) {
-				Write-FudgePackLog -Category "Info" -Message "current startup type is: $($scfg.StartType)"
+			$sst  = $scfg.StartType
+			if ($svcStart -ne "" -and $scfg.StartType -ne $svcStart) {
+				Write-FudgePackLog -Category "Info" -Message "current startup type is: $sst"
 				Write-FudgePackLog -Category "Info" -Message "setting service startup to: $svcStart"
 				Set-Service -Name $svcName -StartupType $svcStart | Out-Null
 			}
-			if ($svcAction -eq 'start') {
-				if ($scfg.Status -ne 'Running') {
-					Write-FudgePackLog -Category "Info" -Message "starting service..."
-					Start-Service -Name $svcName | Out-Null
+			switch ($svcAction) {
+				'start' {
+					if ($scfg.Status -ne 'Running') {
+						Write-FudgePackLog -Category "Info" -Message "starting service..."
+						Start-Service -Name $svcName | Out-Null
+					}
+					else {
+						Write-FudgePackLog -Category "Info" -Message "service is already running"
+					}
+					break
 				}
-				else {
-					Write-FudgePackLog -Category "Info" -Message "service is already running"
+				'restart' {
+					Write-FudgePackLog -Category "Info" -Message "restarting service..."
+					Restart-Service -Name $svcName -ErrorAction SilentlyContinue
+					break
 				}
-			}
+				'stop' {
+					Write-FudgePackLog -Category "Info" -Message "stopping service..."
+					Stop-Service -Name $svcName -Force -NoWait -ErrorAction SilentlyContinue
+					break
+				}
+			} # switch
 		}
 		catch {
 			Write-FudgePackLog -Category "Error" -Message "service not found: $svcName"
@@ -261,7 +282,9 @@ function Invoke-FPFolders {
 	Write-FudgePackLog -Category "Info" -Message "--------- folder assignments ---------"
 	foreach ($folder in $DataSet) {
 		$folderPath  = $folder.path
+		$deviceName  = $folder.device
 		$action = $folder.action
+		Write-FudgePackLog -Category "Info" -Message "assigned to device: $deviceName"
 		Write-FudgePackLog -Category "Info" -Message "folder action assigned: $action"
 		switch ($action) {
 			'create' {
@@ -278,18 +301,40 @@ function Invoke-FPFolders {
 			'empty' {
 				$filter = $folder.filter
 				if ($filter -eq "") { $filter = "*.*" }
-				if ($folder.recurse -eq "true") {
-					Write-FudgePackLog -Category "Info" -Message "deleting $filter from $folderPath and subfolders"
-					Get-ChildItem -Path $folderPath -Filter $filter -Recurse |
-						foreach { Remove-Item -Path $_.FullName -ErrorAction SilentlyContinue }
-					Write-FudgePackLog -Category "Info" -Message "some files may remain if they were in use"
-				}
-				else {
-					Write-FudgePackLog -Category "Info" -Message "deleting $filter from $folderPath only"
-					Get-ChildItem -Path $folderPath -Filter $filter -Recurse |
-						foreach { Remove-Item -Path $_.FullName -ErrorAction SilentlyContinue }
-					Write-FudgePackLog -Category "Info" -Message "some files may remain if they were in use"
-				}
+				Write-FudgePackLog -Category "Info" -Message "deleting $filter from $folderPath and subfolders"
+				Get-ChildItem -Path "$folderPath" -Filter "$filter" -Recurse |
+					foreach { Remove-Item -Path $_.FullName -Confirm:$False -Recurse -ErrorAction SilentlyContinue }
+				Write-FudgePackLog -Category "Info" -Message "some files may remain if they were in use"
+				break
+			}
+		} # switch
+	} # foreach
+}
+
+function Invoke-FPFiles {
+	param (
+		[parameter(Mandatory=$True)]
+		$DataSet
+	)
+	Write-FudgePackLog -Category "Info" -Message "--------- file assignments ---------"
+	foreach ($file in $DataSet) {
+		$fileSource = $file.source
+		$fileTarget = $file.target
+		$action     = $file.action
+		Write-FudgePackLog -Category "Info" -Message "file action assigned: $action"
+		Write-FudgePackLog -Category "Info" -Message "source: $fileSource"
+		Write-FudgePackLog -Category "Info" -Message "target: $fileTarget"
+		switch ($action) {
+			'download' {
+				Write-FudgePackLog -Category "Info" -Message "downloading file"
+				break
+			}
+			'rename' {
+				Write-FudgePackLog -Category "Info" -Message "renaming file"
+				break
+			}
+			'move' {
+				Write-FudgePackLog -Category "Info" -Message "moving file"
 				break
 			}
 		} # switch
@@ -303,13 +348,28 @@ if (-not(Test-Admin)) {
 	Write-Output -2
 	break;
 }
+Write-Verbose "verifying chocolatey installation"
 if (-not(Test-Path "$($env:ProgramData)\chocolatey\choco.exe" )) {
-	iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+	try {
+		Write-Verbose "installing chocolatey"
+		iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+	}
+	catch {
+		Write-Error $_.Exception.Message
+		break
+	}
 }
 
+choco upgrade chocolatey -y
+
+Write-Verbose "preparing to import control file: $ControlFile"
 if ($controlfile.StartsWith("http")) {
 	try {
-		[xml]$controldata = Invoke-RestMethod -Uri $controlfile
+		[xml]$controldata = Invoke-RestMethod -Uri "$controlfile"
+		$logpath = $controldata.configuration.controls.control | 
+			Where-Object {$_.enabled -eq 'true' -and $_.type -eq 'logpath'} |
+				Select-Object -ExpandProperty 'value'
+		if ($logpath -eq "") { $LogFile = $logpath }
 	}
 	catch {
 		Write-FudgePackLog -Category "Error" -Message "failed to import data from Uri: $controlfile"
@@ -335,17 +395,25 @@ else {
 	}
 }
 if ($controldata) {
-	$installs = $controldata.configuration.deployments.deployment | Where-Object {$_.enabled -eq "true" -and $_.device -eq $env:COMPUTERNAME}
-	$removals = $controldata.configuration.removals.removal | Where-Object {$_.enabled -eq "true" -and $_.device -eq $env:COMPUTERNAME}
-	$regkeys  = $controldata.configuration.registry.reg | Where-Object {$_.enabled -eq "true" -and $_.device -eq $env:COMPUTERNAME}
-	$services = $controldata.configuration.services.service | Where-Object {$_.enabled -eq "true" -and $_.device -eq $env:COMPUTERNAME}
-	$folders  = $controldata.configuration.folders.folder | Where-Object {$_.enabled -eq "true" -and $_.device -eq $env:COMPUTERNAME}
-	$files    = $controldata.configuration.files.file | Where-Object {$_.enabled -eq "true" -and $_.device -eq $env:COMPUTERNAME}
-	if ($Payload -eq 'All' -or $Payload -eq 'Folders')  { Invoke-FPFolders -DataSet $folders }
-	if ($Payload -eq 'All' -or $Payload -eq 'Installs') { Invoke-FPChocoInstalls -DataSet $installs }
-	if ($Payload -eq 'All' -or $Payload -eq 'Removals') { Invoke-FPChocoRemovals -DataSet $removals }
-	if ($Payload -eq 'All' -or $Payload -eq 'Registry') { Invoke-FPRegistry -DataSet $regkeys }
-	if ($Payload -eq 'All' -or $Payload -eq 'Services') { Invoke-FPServices -DataSet $services }
+	$mypc = $env:COMPUTERNAME
+	$installs = $controldata.configuration.deployments.deployment | 
+		Where-Object {$_.enabled -eq "true" -and ($_.device -eq $mypc -or $_.device -eq 'all')}
+	$removals = $controldata.configuration.removals.removal | 
+		Where-Object {$_.enabled -eq "true" -and ($_.device -eq $mypc -or $_.device -eq 'all')}
+	$regkeys  = $controldata.configuration.registry.reg | 
+		Where-Object {$_.enabled -eq "true" -and ($_.device -eq $mypc -or $_.device -eq 'all')}
+	$services = $controldata.configuration.services.service | 
+		Where-Object {$_.enabled -eq "true" -and ($_.device -eq $mypc -or $_.device -eq 'all')}
+	$folders  = $controldata.configuration.folders.folder | 
+		Where-Object {$_.enabled -eq "true" -and ($_.device -eq $mypc -or $_.device -eq 'all')}
+	$files    = $controldata.configuration.files.file | 
+		Where-Object {$_.enabled -eq "true" -and ($_.device -eq $mypc -or $_.device -eq 'all')}
+	if ($folders)  { if ($Payload -eq 'All' -or $Payload -eq 'Folders')  { Invoke-FPFolders -DataSet $folders } }
+	if ($installs) { if ($Payload -eq 'All' -or $Payload -eq 'Installs') { Invoke-FPChocoInstalls -DataSet $installs } }
+	if ($removals) { if ($Payload -eq 'All' -or $Payload -eq 'Removals') { Invoke-FPChocoRemovals -DataSet $removals } }
+	if ($regkeys)  { if ($Payload -eq 'All' -or $Payload -eq 'Registry') { Invoke-FPRegistry -DataSet $regkeys } }
+	if ($services) { if ($Payload -eq 'All' -or $Payload -eq 'Services') { Invoke-FPServices -DataSet $services } }
+	if ($files)    { if ($Payload -eq 'All' -or $Payload -eq 'Files')    { Invoke-FPFiles -DataSet $files } }
 }
 Write-FudgePackLog -Category "Info" -Message "---- processing cycle finished ----"
 if ($error.Count -eq 0) { Write-Output 0 } else { Write-Output -1 }
