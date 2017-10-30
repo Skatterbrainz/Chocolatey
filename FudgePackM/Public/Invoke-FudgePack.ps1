a15938c75dc5a34d5e04a245812f6678e1f6780c
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2017-10-30 00:35:19 -0400

    Update Invoke-FudgePack.ps1

--- FudgePackM/Public/Invoke-FudgePack.ps1
+++ FudgePackM/Public/Invoke-FudgePack.ps1
@@ -1,6 +1,5 @@
 #requires -RunAsAdministrator
 #requires -version 3
-
 <#
 .SYNOPSIS
 	Run Chocolatey package assignments and general device config ops using XML config file
@@ -21,46 +20,53 @@
 	default is 'All'
 	
 .NOTES
-	1.0.7 - 10/29/2017 - skatterbrainz
-	
-	Instructions: Drop into $env:ProgramData\fudgepack, run as "system" account via scheduled task (hourly, etc.)
+	0.8.8 - 10/29/2017 - David Stein
 	
 .EXAMPLE
-	
 	Invoke-FudgePack -Verbose
 
 .EXAMPLE
-
 	Invoke-FudgePack -ControlFile "\\server\share\appcontrol.xml"
 
-.EXAMPLE
-	
+.EXAMPLE	
 	Invoke-FudgePack -TestMode -Payload 'Registry' -Verbose
-	
+
+.EXAMPLE
+	Invoke-FudgePack -Configure
 #>
-param (
-	[parameter(Mandatory=$False, HelpMessage="Path or URI to XML control file")]
-		[ValidateNotNullOrEmpty()]
-		[string] $ControlFile = 'https://raw.githubusercontent.com/Skatterbrainz/Chocolatey/master/control.xml',
-	[parameter(Mandatory=$False, HelpMessage="Path to output log file")]
-		[ValidateNotNullOrEmpty()]
-		[string] $LogFile = "$($env:ProgramData)\fudgepack\fudgepack.log",
-	[parameter(Mandatory=$False, HelpMessage="Run in testing mode")]
-		[switch] $TestMode,
-	[parameter(Mandatory=$False, HelpMessage="Specify configuration task group to invoke from XML control file")]
-		[ValidateSet('All','Installs','Removals','Folders','Files','Registry','Services','Configure')]
-		[string] $Payload = 'All'
-)
-. ".\Functions.ps1"
-Write-Verbose "script version: 0.8.7"
-$error.Clear()
-Assert-Chocolatey
-$controlData = Get-FPControlData -FilePath $ControlFile
-if ($controldata) { 
-	Invoke-FPTasks -DataSet $controlData 
+function Invoke-FudgePack {
+	param (
+		[parameter(Mandatory=$False, HelpMessage="Path or URI to XML control file")]
+			[ValidateNotNullOrEmpty()]
+			[string] $ControlFile = 'https://raw.githubusercontent.com/Skatterbrainz/Chocolatey/master/control.xml',
+		[parameter(Mandatory=$False, HelpMessage="Path to output log file")]
+			[ValidateNotNullOrEmpty()]
+			[string] $LogFile = "$($env:TEMP)\fudgepack.log",
+		[parameter(Mandatory=$False, HelpMessage="Run in testing mode")]
+			[switch] $TestMode,
+		[parameter(Mandatory=$False, HelpMessage="Specify configuration task group to invoke from XML control file")]
+			[ValidateSet('All','Installs','Removals','Folders','Files','Registry','Services')]
+			[string] $Payload = 'All',
+		[parameter(Mandatory=$False, HelpMessage="Configure options")]
+			[switch] $Configure
+	)
+	Start-Trancscript
+	Write-Verbose "script version: 0.8.8"
+	$error.Clear()
+	if ($Configure) {
+		Set-FPConfiguration
+	}
+	else {
+		Assert-Chocolatey
+		$controlData = Get-FPControlData -FilePath $ControlFile
+		if ($controldata) { 
+			Invoke-FPTasks -DataSet $controlData 
+		}
+		else {
+			Write-FudgePackLog -Category "Error" -Message "no data was returned from xml request"
+		}
+	}
+	Write-FudgePackLog -Category "Info" -Message "---- processing cycle finished ----"
+	#Stop-Transcript
+	if ($error.Count -eq 0) { Write-Output 0 } else { Write-Output -1 }
 }
-else {
-	Write-FudgePackLog -Category "Error" -Message "no data was returned from xml request"
-}
-Write-FudgePackLog -Category "Info" -Message "---- processing cycle finished ----"
-if ($error.Count -eq 0) { Write-Output 0 } else { Write-Output -1 }
