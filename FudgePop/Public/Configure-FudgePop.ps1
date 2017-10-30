d0e87dba06f2f26203440680ddab9463d85138fc
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2017-10-30 01:25:10 -0400

    Update and rename FudgePacker/Public/Configure-FudgePack.ps1 to FudgePop/Public/Configure-FudgePop.ps1

--- FudgePop/Public/Configure-FudgePop.ps1
+++ FudgePop/Public/Configure-FudgePop.ps1
@@ -5,11 +5,11 @@
 	Prompt for inputs to change scheduled task configuration
 #>
 
-function Configure-FudgePack {
+function Configure-FudgePop {
 	[CmdletBinding()]
 	param ()
 	try {
-		$st = Get-ScheduledTask -TaskName "Run FudgePack" -ErrorAction SilentlyContinue
+		$st = Get-ScheduledTask -TaskName "Run FudgePop" -ErrorAction SilentlyContinue
 		if ($st) {
 			$tr = $st.Triggers
 			$rt = $tr[0].Repetition.Interval # returns PTxH where x = number of hours
@@ -25,21 +25,21 @@ function Configure-FudgePack {
 		Set-FPConfiguration -IntervalHours $yy
 		[string]$choice = (Read-Host -Prompt 'Do you want to run the task now? <Y/N>: ')
 		if ($choice -eq 'Y') {
-			SCHTASKS /Run /TN "Run FudgePack"
+			SCHTASKS /Run /TN "Run FudgePop"
 		}
 	}
 	else {
 		Write-Warning "Invalid interval value entered. Must be between 1 and 12"
 	}
 	try {
-		$logsize = Get-ItemProperty -Path "HKLM:\SOFTWARE\FudgePack" -Name "MaxLogSizeMB" -ErrorAction SilentlyContinue | Select -ExpandProperty MaxLogSizeMB
+		$logsize = Get-ItemProperty -Path "HKLM:\SOFTWARE\FudgePop" -Name "MaxLogSizeMB" -ErrorAction SilentlyContinue | Select -ExpandProperty MaxLogSizeMB
 		$xx = (Read-Host -Prompt 'Maximum log file size in MB [$logsize]')
 		if ($xx -eq $null -or $xx -eq '') { $xx = $logsize }
-		Set-ItemProperty -Path "HKLM:\SOFTWARE\FudgePack" -Name "MaxLogSizeMB" -Value $xx
+		Set-ItemProperty -Path "HKLM:\SOFTWARE\FudgePop" -Name "MaxLogSizeMB" -Value $xx
 	}
 	catch {
 		$xx = (Read-Host -Prompt 'Maximum log file size in MB [10]')
 		if ($xx -eq $null -or $xx -eq '') { $xx = 10 }
-		Set-ItemProperty -Path "HKLM:\SOFTWARE\FudgePack" -Name "MaxLogSizeMB" -Value $xx
+		Set-ItemProperty -Path "HKLM:\SOFTWARE\FudgePop" -Name "MaxLogSizeMB" -Value $xx
 	}
 }
